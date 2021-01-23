02aa8f9225977e7496c2dcd209ab1ee2f3c6c579
Matt <hallmatthewd12@gmail.com>
2021-01-23 18:11:31 -0500

    Part 7 (Fixed Key Input)

--- src/com/tutorial/main/KeyInput.java
+++ src/com/tutorial/main/KeyInput.java
@@ -6,9 +6,16 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter{
 	
 	private Handler handler;
+	private boolean[] keyDown = new boolean[4];
 	
 	public KeyInput(Handler handler) {
 		this.handler = handler;
+		
+		keyDown[0]=false;
+		keyDown[1]=false;
+		keyDown[2]=false;
+		keyDown[3]=false;
+		
 	}
 	
 	public void keyPressed(KeyEvent e) {
@@ -20,10 +27,10 @@ public class KeyInput extends KeyAdapter{
 			if(tempObject.getId() == ID.Player) {     // Only goes into the "if" statement if the object has the ID of "Player"
 				//key events for player 1
 				
-				if(key == KeyEvent.VK_W) tempObject.setVelY(-5);     // Check if we're hitting W, and move up
-				if(key == KeyEvent.VK_S) tempObject.setVelY(5);      // Check if we're hitting S, and move down
-				if(key == KeyEvent.VK_D) tempObject.setVelX(5);      // Check if we're hitting D, and move right
-				if(key == KeyEvent.VK_A) tempObject.setVelX(-5);     // Check if we're hitting A, and move left
+				if(key == KeyEvent.VK_W) {tempObject.setVelY(-5); keyDown[0]=true; }     // Check if we're hitting W, and move up
+				if(key == KeyEvent.VK_S) {tempObject.setVelY(5); keyDown[1]=true; }      // Check if we're hitting S, and move down
+				if(key == KeyEvent.VK_D) {tempObject.setVelX(5); keyDown[2]=true; }      // Check if we're hitting D, and move right
+				if(key == KeyEvent.VK_A) {tempObject.setVelX(-5); keyDown[3]=true; }     // Check if we're hitting A, and move left
 			}
 		}
 		
@@ -39,10 +46,15 @@ public class KeyInput extends KeyAdapter{
 			if(tempObject.getId() == ID.Player) {     // Only goes into the "if" statement if the object has the ID of "Player"
 				//key events for player 1
 				
-				if(key == KeyEvent.VK_W) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(0);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(0);
+				if(key == KeyEvent.VK_W) keyDown[0]=false; 
+				if(key == KeyEvent.VK_S) keyDown[1]=false;
+				if(key == KeyEvent.VK_D) keyDown[2]=false; 
+				if(key == KeyEvent.VK_A) keyDown[3]=false; 
+				
+				//vertical movement
+				if(!keyDown[0] && !keyDown[1]) tempObject.setVelY(0);
+				//horizontal movement
+				if(!keyDown[2] && !keyDown[3]) tempObject.setVelX(0);
 			}
 		}
 	}
